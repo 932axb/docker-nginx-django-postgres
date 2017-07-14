8c46515ddca5cfad74bee35b5ad0a8950b36bc8e
vlad <vlad.execve@gmail.com>
2017-07-14 14:58:47 +0600

    add bin scripts

---
+++ frontend-static/static/entry.js
@@ -0,0 +1,8 @@
+require("./src/js/main.js")
+
+require("./src/sass/main.sass")
+require("./src/css/main.css")
+
+require("./node_modules/bootstrap-v4-dev/dist/css/bootstrap.css")
+require("./node_modules/bootstrap-v4-dev/dist/js/bootstrap.js")
+
